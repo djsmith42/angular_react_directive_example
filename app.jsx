d1c01e0959fda1565f0f8cee542280dd5ed8917b
Dave Smith <dsmith@hirevue.com>
2015-02-26 16:51:31 -0700

    Better results display with bg colors
    And shared random timeout generator so we can show it better.

--- app.jsx
+++ app.jsx
@@ -5,6 +5,10 @@
 var HOURS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 var DAYS  = ["1 October", "2 October", "3 October", "4 October", "5 October", "6 October", "7 October", "8 OCtober", "9 October", "10 October", "11 October", "12  October", "13 October", "14 October", "15 October", "16 October", "17 October", "18 October", "19 October", "20 October"] //, "21 October", "22 October", "23 October", "24 October", "25 October", "26 October", "27 October", "28 October", "29 October", "30 October", "31 October"];
 
+var randomMillis = function() {
+  return 0000 + Math.floor(Math.random() * 5000);
+}
+
 angular.module("myapp", []).
 
 directive("myCalendarCell", function() {
@@ -62,7 +66,7 @@ directive("myCalendarCell", function() {
           $timeout(function() {
             $scope.status.isSearching = false;
             $scope.status.searchResults = {options: Math.floor(Math.random() * 5)};
-          }, 100 + Math.random() * 500)
+          }, randomMillis());
         }
       }
       $scope.$on('daySearchRequested', function(event, day) {
@@ -130,7 +134,7 @@ directive("myCalendarReact", function() {
             isSearching: false,
             searchResults: {options: Math.floor(Math.random() * 5)}
           });
-        }, 100 + Math.random() * 500)
+        }, randomMillis());
       }
 
       var HeaderCell = React.createClass({
@@ -161,8 +165,8 @@ directive("myCalendarReact", function() {
             'time'        : !this.state.isSearching && options === undefined,
             'searching'   : this.state.isSearching,
             'good-results': options && options > 3,
-            'weak-results': options && options > 1 && options < 3,
-            'bad-results' : options && options >= 0 && options <= 1
+            'weak-results': options && options > 1 && options <= 3,
+            'bad-results' : options === 0 || options === 1
           });
           if (this.state.isSearching) {
             return (
