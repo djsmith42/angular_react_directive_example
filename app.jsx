6e6f3deea7d1bda4f74abd15643add023aa61e68
Dave Smith <dsmith@hirevue.com>
2015-03-02 23:01:53 -0700

    More demo progress
     - Deeply nested <div>s in each cell
     - Factor React componets out of the link() method
     - More idiomatic JSX
     - Squash some React console warnings.

--- app.jsx
+++ app.jsx
@@ -3,10 +3,10 @@
  */
 
 var HOURS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
-var DAYS  = ["1 October", "2 October", "3 October", "4 October", "5 October", "6 October", "7 October", "8 October", "9 October", "10 October", "11 October", "12  October", "13 October", "14 October", "15 October", "16 October", "17 October", "18 October", "19 October", "20 October"] //, "21 October", "22 October", "23 October", "24 October", "25 October", "26 October", "27 October", "28 October", "29 October", "30 October", "31 October"];
+var DAYS  = ["1 October", "2 October", "3 October", "4 October", "5 October", "6 October", "7 October", "8 October", "9 October", "10 October", "11 October", "12  October", "13 October", "14 October", "15 October", "16 October", "17 October", "18 October", "19 October", "20 October", "21 October", "22 October", "23 October", "24 October", "25 October", "26 October", "27 October", "28 October", "29 October", "30 October", "31 October"];
 
 var randomMillis = function() {
-  return Math.floor(Math.random() * 5000);
+  return Math.floor(Math.random() * 500);
 }
 
 angular.module("myapp", []).
@@ -17,8 +17,9 @@ directive("myCalendar", function() {
         replace: true,
         template:
             '<div class="calendar">' +
-            ' <button class="btn" ng-click="searchAll()">Search all month</button>' +
-            ' <table>' +
+            ' <button class="btn" ng-hide="loaded" ng-click="load()">Load</button>' +
+            ' <button class="btn" ng-show="loaded" ng-click="searchAll()">Search all month</button>' +
+            ' <table ng-if="loaded">' +
             '  <tr>' +
             '   <th ng-repeat="day in days" class="day-header" ng-click="dayHeaderClicked(day)">' +
             '     {{day}}' +
@@ -32,6 +33,7 @@ directive("myCalendar", function() {
             ' </table>' +
             '</button>',
         link: function(scope, element, attrs) {
+            scope.loaded = false;
             scope.hours = HOURS;
             scope.days  = DAYS;
 
@@ -41,6 +43,10 @@ directive("myCalendar", function() {
 
             scope.dayHeaderClicked = function(day) {
               scope.$broadcast('daySearchRequested', day);
+            }
+
+            scope.load = function() {
+              scope.loaded = true;
             }
         }
     }
@@ -53,14 +59,48 @@ directive("myCalendarCell", function() {
     template:
       '<div ng-click="cellClicked(day, hour)" ng-class="cellClass()">' +
       '  <div ng-if="showSpinner()">' +
-      '    Searching' +
+      '    <div ng-if="showSpinner()">' +
+      '      <div ng-if="showSpinner()">' +
+      '        <div ng-if="showSpinner()">' +
+      '          <div ng-if="showSpinner()">' +
+      '            <div ng-if="showSpinner()">' +
+      '              Searching' +
+      '            </div>' +
+      '          </div>' +
+      '        </div>' +
+      '      </div>' +
+      '    </div>' +
       '  </div>' +
       '  <div ng-if="showHour()" class="time">' +
-      '    {{hour}}:00' +
+      '    <div ng-if="showHour()">' +
+      '      <div ng-if="showHour()">' +
+      '        <div ng-if="showHour()">' +
+      '          <div ng-if="showHour()">' +
+      '            <div ng-if="showHour()">' +
+      '              <div ng-if="showHour()">' +
+      '                {{hour}}:00' +
+      '              </div>' +
+      '            </div>' +
+      '          </div>' +
+      '        </div>' +
+      '      </div>' +
+      '    </div>' +
       '  </div>' +
       '  <div ng-if="showSearchResults()">' +
-      '    <div>{{status.searchResults.options}}</div>' +
-      '    <div>results</div>' +
+      '    <div ng-if="showSearchResults()">' +
+      '      <div ng-if="showSearchResults()">' +
+      '        <div ng-if="showSearchResults()">' +
+      '          <div ng-if="showSearchResults()">' +
+      '            <div ng-if="showSearchResults()">' +
+      '              <div ng-if="showSearchResults()">' +
+      '                <div>{{status.searchResults.options}}</div>' +
+      '                <div>results</div>' +
+      '              </div>' +
+      '            </div>' +
+      '          </div>' +
+      '        </div>' +
+      '      </div>' +
+      '    </div>' +
       '  </div>' +
       '</div>',
     link: function(scope, element, attrs) {
@@ -68,7 +108,7 @@ directive("myCalendarCell", function() {
       scope.hour = attrs.hour;
       scope.status = {};
     },
-    controller: function($scope, $timeout, $rootScope) {
+    controller: function($scope, $rootScope, $timeout) {
       $scope.showSpinner = function() {
         return $scope.status.isSearching;
       }
@@ -92,16 +132,15 @@ directive("myCalendarCell", function() {
         }
       }
       $scope.cellClicked = function() {
-        var alreadySearching = $scope.status.isSearching;
         delete $scope.status.searchResults;
-        $scope.status.isSearching = !alreadySearching;
-        if (!alreadySearching) {
-          // Simulate an AJAX request:
-          $timeout(function() {
-            $scope.status.isSearching = false;
-            $scope.status.searchResults = {options: Math.floor(Math.random() * 5)};
-          }, randomMillis());
-        }
+        $scope.status.isSearching = true;
+        // Simulate an AJAX request:
+        //setTimeout(function() {
+        $timeout(function() {
+          $scope.status.isSearching = false;
+          $scope.status.searchResults = {options: Math.floor(Math.random() * 5)};
+          //$scope.$digest();
+        }, randomMillis());
       }
       $scope.$on('daySearchRequested', function(event, day) {
         if (day == $scope.day) {
@@ -120,136 +159,171 @@ directive("myCalendarReact", function() {
     scope: {},
     template: '<div></div>',
     link: function(scope, element, attrs) {
-      var cellCache = {};
-      for (var i=0; i<DAYS.length; i++) {
-        cellCache[DAYS[i]] = {}
-      }
-
-      function _searchCell(day, hour) {
-        var cell = cellCache[day][hour];
-        cell.setState({isSearching: true, searchResults: null});
-        setTimeout(function() {
-          cell.setState({
-            isSearching: false,
-            searchResults: {options: Math.floor(Math.random() * 5)}
-          });
-        }, randomMillis());
-      }
-
-      var HeaderCell = React.createClass({
-        render: function() {
-          return (<th className='day-header' onClick={this.clicked}>{this.props.day}</th>)
-        },
-        clicked: function() {
-          for (var i=0; i < HOURS.length; i++) {
-            _searchCell(this.props.day, HOURS[i]);
-          }
-        }
-      });
-
-      var HeaderRow = React.createClass({
-        render: function() {
-          var headerCells = [];
-          for (var i=0; i < DAYS.length; i++) {
-            headerCells.push(HeaderCell({day: DAYS[i]}));
-          }
-          return (<tr>{headerCells}</tr>);
-        }
-      });
-
-      var Cell = React.createClass({
-        render: function() {
-          var options = this.state.searchResults && this.state.searchResults.options;
-          var classes = React.addons.classSet({
-            'time'        : !this.state.isSearching && options === undefined,
-            'searching'   : this.state.isSearching,
-            'good-results': options && options > 3,
-            'weak-results': options && options > 1 && options <= 3,
-            'bad-results' : options === 0 || options === 1
-          });
-          if (this.state.isSearching) {
-            return (
-              <td className='hour-cell'>
-                <div className={classes}>Searching</div>
-              </td>
-              );
-          } else if (this.state.searchResults) {
-            return (
-              <td className='hour-cell' onClick={this.clicked}>
-                <div className={classes}>
-                  <div>{this.state.searchResults}</div>
-                  <div>results</div>
-                </div>
-              </td>
-              );
-          } else {
-            return (
-              <td className='hour-cell' onClick={this.clicked}>
-                <div className={classes}>
-                  {this.props.hour}:00
-                </div>
-              </td>
-              );
-          }
-        },
-        getInitialState: function() {
-          return {
-            isSearching: false
-          }
-        },
-        componentDidMount: function() {
-          cellCache[this.props.day][this.props.hour] = this;
-        },
-        clicked: function() {
-          var props = this.props;
-          if (!this.state.isSearching) {
-            _searchCell(props.day, props.hour);
-          }
-        }
-      });
-
-      var Row = React.createClass({
-        render: function() {
-          var cells = [];
-          for (var i=0; i < DAYS.length; i++) {
-            var day = DAYS[i];
-            var cell = Cell({hour: this.props.hour, day: day});
-            cells.push(cell);
-          }
-          return (<tr>{cells}</tr>)
-        }
-      });
-
-      var Calendar = React.createClass({
-        render: function() {
-          var headerRow = HeaderRow();
-
-          var rows = [];
-          for (var i=0; i < HOURS.length; i++) {
-            var hour = HOURS[i];
-            rows.push(Row({hour: hour}));
-          }
-
-          return (
-            <div className='calendar'>
-              <button className='btn' onClick={this.searchAll}>Search all month</button>
-              <table>
-                <HeaderRow />
-                {rows}
-              </table>
-            </div>
-            )
-        },
-        searchAll: function() {
-          for (var i=0; i < HOURS.length; i++) {
-            for (var j=0; j < DAYS.length; j++) {
-              _searchCell(DAYS[j], HOURS[i]);
-            }
-          }
-        }
-      });
-
-      React.renderComponent(Calendar(), element[0]);
+      var cells = []
+      React.render(<Calendar cells={cells}/>, element[0]);
     }
+  }
+});
+
+
+/* React Components */
+
+var HeaderCell = React.createClass({
+  render: function() {
+    return (
+      <th className='day-header' onClick={this.clicked}>{this.props.day}</th>
+    )
+  }
+});
+
+var HeaderRow = React.createClass({
+  render: function() {
+    return (
+      <tr>
+        {DAYS.map(function(day) {
+          return <HeaderCell day={day} key={day} />
+        })}
+      </tr>
+      );
+  }
+});
+
+var Cell = React.createClass({
+  search: function() {
+    var self = this;
+    self.setState({
+      isSearching: true,
+      searchResults: {options: null}
+    });
+    setTimeout(function() {
+      self.setState({
+        isSearching: false,
+        searchResults: {options: Math.floor(Math.random() * 5)}
+      });
+    }, randomMillis());
+  },
+  componentWillMount: function() {
+    this.props.cells.push(this);
+  },
+  render: function() {
+    var options = this.state.searchResults && this.state.searchResults.options;
+    var classes = React.addons.classSet({
+      'time'        : !this.state.isSearching && options === undefined,
+      'searching'   : this.state.isSearching,
+      'good-results': options && options > 3,
+      'weak-results': options && options > 1 && options <= 3,
+      'bad-results' : options === 0 || options === 1
+    });
+    if (this.state.isSearching) {
+      return (
+        <td className='hour-cell'>
+          <div className={classes}>
+            <div>
+              <div>
+                <div>
+                  <div>
+                    <div>
+                      <div>
+                        Searching
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </td>
+      );
+    } else if (this.state.searchResults) {
+      return (
+        <td className='hour-cell' onClick={this.clicked}>
+          <div className={classes}>
+            <div>
+              <div>
+                <div>
+                  <div>
+                    <div>
+                      <div>{this.state.searchResults}</div>
+                      <div>results</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </td>
+      );
+    } else {
+      return (
+        <td className='hour-cell' onClick={this.clicked}>
+          <div className={classes}>
+            <div>
+              <div>
+                <div>
+                  <div>
+                    {this.props.hour}:00
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </td>
+      );
+    }
+  },
+  getInitialState: function() {
+    return {
+      isSearching: false
+    }
+  },
+  clicked: function() {
+    this.search();
+  }
+});
+
+var Row = React.createClass({
+  render: function() {
+    var hour = this.props.hour;
+    var cells = this.props.cells;
+    return (
+      <tr>
+        {DAYS.map(function(day) {
+          return <Cell hour={hour} day={day} key={day} cells={cells} />
+          })}
+      </tr>
+    )
+  }
+});
+
+var Calendar = React.createClass({
+  getInitialState: function() {
+    return {
+      isLoaded: false
+    }
+  },
+  load: function() {
+    this.setState({isLoaded: true});
+  },
+  render: function() {
+    var cells = this.props.cells;
+    return (
+      <div className='calendar'>
+        {this.state.isLoaded || <button className='btn' onClick={this.load}>Load</button>}
+        {this.state.isLoaded && <button className='btn' onClick={this.searchAll}>Search all month</button>}
+        {this.state.isLoaded &&
+        <table>
+          <HeaderRow />
+          {HOURS.map(function(hour) {
+            return <Row hour={hour} key={hour} cells={cells}/>
+          })}
+        </table>
+        }
+      </div>
+      )
+  },
+  searchAll: function(args) {
+    this.props.cells.forEach(function(cell) {
+      cell.search();
+    });
   }
 });
